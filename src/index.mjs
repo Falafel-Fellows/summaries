c54d8a55fb41812fa8ac3eee6c134bf3ecaac329
M. Quasar <marcosm@gmail.com>
2023-08-16 23:45:16 -0300

    fix: added timestamps and a prompt using them

--- src/index.mjs
+++ src/index.mjs
@@ -44,15 +44,23 @@ const chunkLength = 35000
 
 const totalSubtitleSize = fs.statSync(subtitleFile).size
 const numberOfChunks = Math.ceil(totalSubtitleSize / chunkLength)
-const chunkStarts = Array(numberOfChunks).fill(null).map((item, index) => index*chunkLength )
-
+const chunkStarts = Array(numberOfChunks).fill(null).map((item, index) => index*chunkLength)
+let timestamps = []
+// TODO: figure out why order of the chunks are not preserved
 Promise.all(chunkStarts.map(async (startPosition, partNumber) => {
-  const systemMessage = `You are receiving the subtitles of part ${partNumber} of a work session between 3 fellows and you have to summarize it.`
+  const systemMessage = `Here is the part number ${partNumber}, your job is to summarize it`
   const textToAnalyze = (await readChunk(subtitleFile, {length: chunkLength, startPosition})).toString();
+  const timestampRegex = /(\d{2}:\d{2}:\d{2}\.\d{3})/;
+  // Find the first match using the regex
+  const firstTimestampMatch = textToAnalyze.match(timestampRegex);
+
+  // Extract the timestamp if a match was found
+  const firstTimestamp = firstTimestampMatch ? firstTimestampMatch[1] : null;
+  timestamps.push(firstTimestamp)
   return summarize(textToAnalyze, systemMessage)
 })).then(async summaries => {
-  const summariesText = summaries.reduce((acc, curr) => {
-    return `${acc}\n ${curr.data.choices[0].message.content}`
+  const summariesText = summaries.reduce((acc, curr, index) => {
+    return `${acc}\n ${timestamps[index]}\n ${curr.data.choices[0].message.content}`
   }, "")
   console.log(summariesText)
   const systemMessage = `You summarize text, the provided input will be parts of a meeting.`
